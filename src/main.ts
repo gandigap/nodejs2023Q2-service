5d5ba3ae9fcfcbad105296de22d529dd003a862e
Ihar Berasneu <iberesnev@alfabank.ru>
2023-07-31 23:48:41 +0300

    feat: add swager and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import * as yaml from 'js-yaml';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 const PORT = process.env.PORT;
 
@@ -11,6 +15,13 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const document = yaml.load(
+    readFileSync(join(__dirname, '..', 'doc', 'api.yaml'), 'utf8'),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(PORT ? +PORT : 3000);
 }
 bootstrap();
